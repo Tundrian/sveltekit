03cf4c2ad529369c6e771fc73d29951607b0aaf6
Antonio <antoniosansotta@gmail.com>
2023-04-05 21:30:42 -0400

    1:12:52

---
+++ src/routes/posts/create/+error.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { page } from '$app/stores'
+</script>
+
+<h1>Error Encountered</h1>
+
+<h1>{$page.error?.message}</h1>
+<p>Something went horribly wrong</p>
+
+<pre>
+    {JSON.stringify($page, null, 2)}
+</pre>
